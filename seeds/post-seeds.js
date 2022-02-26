b1ff7dc7e84763643d62f1fe3fd0c650daecd1b2
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-02-26 15:48:59 -0600

    changed up css, all routes working except for singlepost

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,35 +2,35 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    post_content: 'Lorem ipsum dolor sit amet.',
+    title: 'Consectetur adipiscing elit!',
+    post_content: 'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     user_id: 1 
   },
-//   {
-//     title: 'Interdum varius sit amet mattis vulputate enim.',
-//     post_content: 'Sem integer vitae justo eget magna fermentum iaculis eu non. Ornare suspendisse sed nisi lacus sed. Odio eu feugiat pretium nibh ipsum consequat nisl vel.',
-//     user_id: 1 
-//   },
-//   {
-//     title: 'Egestas congue quisque egestas diam.',
-//     post_content: 'Aliquam sem et tortor consequat id porta nibh venenatis. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit.',
-//     user_id: 1
-//   },
-//   {
-//     title: 'Viverra tellus in hac habitasse platea dictumst vestibulum.',
-//     post_content: 'Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Ut eu sem integer vitae justo eget magna. Nec ullamcorper sit amet risus nullam eget felis eget nunc.',
-//     user_id: 1 
-//   },
   {
-    title: 'Sit amet facilisis magna etiam.',
+    title: 'Interdum varius sit amet mattis.',
+    post_content: 'Sem integer vitae justo eget magna fermentum iaculis eu non. Ornare suspendisse sed nisi lacus sed. Odio eu feugiat pretium nibh ipsum consequat nisl vel.',
+    user_id: 1 
+  },
+  {
+    title: 'Egestas congue.',
+    post_content: 'Aliquam sem et tortor consequat id porta nibh venenatis. In hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit.',
+    user_id: 1
+  },
+  {
+    title: 'Viverra tellus in hac.',
+    post_content: 'Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Ut eu sem integer vitae justo eget magna. Nec ullamcorper sit amet risus nullam eget felis eget nunc.',
+    user_id: 1 
+  },
+  {
+    title: 'Sit amet facilisis.',
     post_content: 'Molestie nunc non blandit massa enim nec dui.',
     user_id: 2 
   },
-//   {
-//     title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-//     post_content: 'Pellentesque massa placerat duis ultricies lacus sed. Id aliquet lectus proin nibh nisl condimentum id venenatis. Elementum sagittis vitae et leo duis ut diam quam nulla. Lobortis elementum nibh tellus molestie nunc non blandit. Justo eget magna fermentum iaculis eu non.',
-//     user_id: 2 
-//   }
+  {
+    title: 'Lorem ipsum dolor.',
+    post_content: 'Pellentesque massa placerat duis ultricies lacus sed. Id aliquet lectus proin nibh nisl condimentum id venenatis. Elementum sagittis vitae et leo duis ut diam quam nulla. Lobortis elementum nibh tellus molestie nunc non blandit. Justo eget magna fermentum iaculis eu non.',
+    user_id: 2 
+  }
 ];
 
 const seedPosts = () => Post.bulkCreate(postdata);
