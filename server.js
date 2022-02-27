146ac99aaebd2982a1071b94c051dde0f7562bc2
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2022-02-26 19:42:00 -0600

    add secret to account for heroku error

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.sess_secret,
+    secret: veryverysecret,
     cookie: {},
     resave: false,
     saveUninitialized: true,
